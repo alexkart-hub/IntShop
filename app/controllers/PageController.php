9bea732d763d443b460634c090cb788a744bfcc5
alexkart-hub <57880131+alexkart-hub@users.noreply.github.com>
2019-12-13 06:44:19 +0300

    Закончил написание роутера

---
+++ app/controllers/PageController.php
@@ -0,0 +1,15 @@
+<?php 
+namespace app\controllers;
+
+class PageController
+{
+    public function indexAction()
+    {
+        echo __METHOD__;
+    } 
+    public function viewAction()
+    {
+        echo __METHOD__;
+    } 
+   
+}
--- public/index.php
+++ public/index.php
@@ -2,12 +2,13 @@
 
 use ishop\App;
 use ishop\Registry;
+use ishop\Router;
 
 require_once dirname(__DIR__).'/config/init.php';
 require_once LIBS.'/functions.php';
+require_once CONF.'/routes.php';
 
 $app = new App;
 $app::$app -> setProperty('Sancho','Panso');
 // debug($app::$app->getProperties());
 
-// throw new Exception('Страница не найдена',500);
